5e9f659eb2ad68155ef8105869d26a5352726438
Troy Shu <tmshu1@gmail.com>
2017-03-05 10:38:04 -0500

    Revert "fix some conflicting auto layout constraints"
    This reverts commit 208103aca9d2230761088d160e50548d8fc4dfeb.

--- SwiftStockExample/ViewController.swift
+++ SwiftStockExample/ViewController.swift
@@ -102,6 +102,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     func keyboardWillHide(_ sender: Notification) {
         if let userInfo = sender.userInfo {
             // if let _ = (userInfo[UIKeyboardFrameEndUserInfoKey] as AnyObject).cgRectValue.size.height { // commenting out because of "Initializer for conditional binding must have Optional type", which occurs if the RHS is not optional
+                tableViewBottomConstraint.constant = 0.0
                 UIView.animate(withDuration: 0.25, animations: { () -> Void in
                     self.view.layoutIfNeeded()
                 })
